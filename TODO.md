fb69802f1c563d5fcc743113c041ad6bb1c9cff3
V. David Zvenyach <dave@zvenyach.com>
2022-12-18 15:42:04 -0600

    :sparkles: Silent mode!

--- TODO.md
+++ TODO.md
@@ -11,3 +11,4 @@
 ## Done
 - [X] [Tests](https://github.com/vdavez/habits/commit/f571b5c4f59052b8d3a042c1db39c061e7baa266)
 - [X] [Handle upserts for things to adjust as the day goes on...](https://github.com/vdavez/habits/commit/46b96038bf5564044fd4a566f606856fccf9af1f)
+- [X] [A silent mode if today's been tracked...]
--- run.sh
+++ run.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
+# file: run.sh
 
-if [[ ! -z "$1" ]]
-then 
-    if [ "$1" == "test" ]; then DB="tests/test.db"; else DB="$1"; fi
+if [[ ! -z "$1" ]]; then
+    # Check if test DB
+    if [[ "$1" == "test" ]];then DB="tests/test.db";
+    else
+        # Check if silent mode
+        if [[ "$1" == "-s" ]]; then
+            SILENT=1
+
+            # Check if user-defined DB
+            if [[ ! -z "$2" ]]; then 
+                DB="$2";
+            else
+                DB="habits.db"
+            fi
+        else
+            DB=$1
+        fi
+    fi
 else
     DB="habits.db"
 fi
@@ -11,6 +27,9 @@ fi
 current=$( sqlite3 $DB 'select * from today;' )
 weight_input=$(awk -F '|' '{print $1}'<<< "$current")
 medicine_input=$(awk -F '|' '{print $2}'<<< "$current")
+
+# If there's a recording and "-s" is used, exit 
+if [[ ! -z "$weight_input" ]] && [[ ! -z "$SILENT" ]]; then exit 0; fi
 
 read -p "What did you weigh in at this morning? [$weight_input]: " weight
 if [[ -z "$weight" ]]  && [[ ! -z "$weight_input" ]]
